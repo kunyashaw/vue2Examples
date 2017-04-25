8e7707e5615ef8b23e5cdf5733096918075e2de2
kunyashaw <zhang_zhong_lei@163.com>
2017-04-25 13:16:02 +0800

    add http

---
+++ src/components/demo05.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+    <span>{{"msg is "+msg}}</span>
+    <button v-on:click="getData()">getData</button>
+</div>
+</template>
+
+<script>
+    export default{
+        name:'demo05',
+        data(){
+            return {
+                msg:''
+            }
+        },
+        methods:{
+            getData(){
+
+                this.$http.get('static/test.json')
+                .then(function(response){
+                    console.log(response);
+                    console.log(response.data);
+                    this.msg = response.data[0].name;
+                })
+            }
+        }
+    }    
+</script>
+
+<style lang="">
+    
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import demo01 from '@/components/demo01'
 import demo02 from '@/components/demo02'
 import demo03 from '@/components/demo03'
 import demo04 from '@/components/demo04'
+import demo05 from '@/components/demo05'
+
 Vue.use(Router)
 
 export default new Router({
@@ -34,7 +36,10 @@ export default new Router({
       name: 'demo04',
       component:demo04
     },
-    
-    
+           {
+      path: '/demo05',
+      name: 'demo05',
+      component:demo05
+    }
   ]
 })
